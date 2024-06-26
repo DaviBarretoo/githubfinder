9fe908c3ebf3d2a00dd3f0b0631e78f569bf1325
Davi Barreto <davibarretojkda@gmail.com>
2024-06-26 13:30:01 -0300

    80%

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,17 +1,25 @@
 import Search from "../components/Search"
 import { useState } from "react"
 import { UserProps } from "../types/user"
+import User from "../components/User"
+import Error from "../components/Error"
 
 export default function Home (){
     const [user, setUser]= useState<UserProps | null>(null)
-
+const [error, setError] = useState(false)
     // Logando a API
 
     const loadUser = async(userName: string) => {
+        setError(false)
+        setUser(null)
         const res = await fetch(`https://api.github.com/users/${userName}`)
 
         const data = await res.json()
 
+        if(res.status === 404) {
+            setError(true)
+            return;
+        }
         // Pegando somente o que eu vou usar do objeto recebido do Data
     const { avatar_url, login, location,followers,following} = data
    
@@ -29,7 +37,9 @@ export default function Home (){
     return(
         <div>
         <Search loadUser={loadUser}/>
-        {user && <p>{user.login}</p>}
+         {/* usando Spreadoperator para "espalhar as info do user" */}
+        {user && <User {...user}/>}
+        {error && <Error/>}
         </div>
     )
 
